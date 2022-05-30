bf57b46b68e68df56d00c5f5d3131772597d21b7
Samir <46886180+datagig2408@users.noreply.github.com>
2022-05-30 10:23:23 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("******************* This is my first program webhook! *****************"); 
+        System.out.println("******************* This is my first program - test Polling! *****************"); 
     }
 }
